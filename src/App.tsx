dbb7b0392a360e3ac497c6e6fa47cd61ce06275a
Henrique Fadoni <henrique.fadoni@blindside.pro>
2019-12-24 21:59:34 -0300

    Pass List with data to ListWidget
    Correct the Type of the properties received in the Component

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,40 @@ import React, { FunctionComponent } from 'react'
 import ListWidget from './components/ListWidget/ListWidget'
 import DropBox from './components/DropBox/DropBox'
 
-const listExample = ['List 1', 'List 2', 'List 3'];
+const listExample = [
+  {
+    name: 'List 1',
+    data: [
+      { time: '2019-04-11', value: 80.01 },
+      { time: '2019-04-12', value: 96.63 },
+      { time: '2019-04-13', value: 76.64 },
+      { time: '2019-04-14', value: 81.89 },
+      { time: '2019-04-15', value: 74.43 },
+      { time: '2019-04-16', value: 80.01 }
+    ]
+  },
+  {
+    name: 'List 2',
+    data: [
+      { time: '2019-04-17', value: 96.63 },
+      { time: '2019-04-18', value: 76.64 },
+      { time: '2019-04-19', value: 81.89 },
+      { time: '2019-04-20', value: 74.43 }
+    ]
+  },
+  {
+    name: 'List 3',
+    data: [
+      { time: '2019-04-13', value: 76.64 },
+      { time: '2019-04-14', value: 81.89 },
+      { time: '2019-04-15', value: 74.43 }
+    ]
+  }
+];
 
 const App: FunctionComponent = () => (
   <div>
-    <ListWidget list={listExample} />
+    <ListWidget chartList={listExample} />
     <DropBox />
     <DropBox />
   </div>
--- src/components/ListWidget/ListWidget.tsx
+++ src/components/ListWidget/ListWidget.tsx
@@ -1,17 +1,35 @@
 import React, { FunctionComponent, memo } from 'react'
 
+import { selectList } from '../../store/actions/index'
+
 import DropBox from '../DropBox/DropBox'
 import Widget from '../Widget/Widget'
 
-interface ListWidgetProps {
-  list: string[]
+interface ChartData {
+  time: string,
+  value: number
 }
 
+interface Chart {
+  name: string,
+  data: Array<ChartData>
+}
+
+interface ListWidgetProps {
+  chartList: Array<Chart>
+}
+
+const ListWidget: FunctionComponent<ListWidgetProps> = ({ chartList }) => {
+  list: string[]
+  }
+
+  const newRenderList = chartList.map((item, index) => (
 // Add On Click Event => Changes Redux State (Selected Item)
 const ListWidget: FunctionComponent<ListWidgetProps> = ({ list }) => {
   const newRenderList = list.map((item, index) => (
     <div key={index}>
       <h3>{item}</h3>
+      <h3>{item.name}</h3>
     </div>
   ));
 
