e90258d154773a54067705558dd9793bc7e69b12
Henrique Fadoni <henrique.fadoni@blindside.pro>
2019-12-27 17:46:49 -0300

    Connect Chart to Redux

--- src/components/ChartWidget/Chart/Chart.tsx
+++ src/components/ChartWidget/Chart/Chart.tsx
@@ -1,29 +1,22 @@
-import React, { FunctionComponent, memo, useEffect } from 'react'
+import React, { FunctionComponent, memo, useEffect, useState } from 'react'
+import { useSelector } from 'react-redux'
 
 import { createChart } from 'lightweight-charts';
 
 const Chart: FunctionComponent = () => {
+  const { selectedList } = useSelector((state: any) => state)
+
   useEffect(() => {
-    const selectChartDiv = document.querySelector('#chart-root');
-    // @ts-ignore
-    const chart = createChart(selectChartDiv, { width: 400, height: 300 });
-    const lineSeries = chart.addLineSeries();
+    if (selectedList && selectedList.data) {
+      const selectChartDiv = document.querySelector('#chart-root')
+        // @ts-ignore
+        const chart = createChart(selectChartDiv, { width: 400, height: 300 })
+        const lineSeries = chart.addLineSeries()
+        lineSeries.setData(selectedList.data)
+    }
+  }, [selectedList]);
 
-    lineSeries.setData([
-      { time: '2019-04-11', value: 80.01 },
-      { time: '2019-04-12', value: 96.63 },
-      { time: '2019-04-13', value: 76.64 },
-      { time: '2019-04-14', value: 81.89 },
-      { time: '2019-04-15', value: 74.43 },
-      { time: '2019-04-16', value: 80.01 },
-      { time: '2019-04-17', value: 96.63 },
-      { time: '2019-04-18', value: 76.64 },
-      { time: '2019-04-19', value: 81.89 },
-      { time: '2019-04-20', value: 74.43 },
-    ]);
-  }, []);
-
-  return <div id='chart-root'></div>
+  return selectedList ? <div id='chart-root'></div> : <h3>Select a List</h3>
 }
 
 export default memo(Chart)
